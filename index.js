82e70cb4f3876b61ebebda71f72e33847ba8a80d
poppy47 <sukhdevps04@gmail.com>
2026-05-08 17:48:11 +0530

    adding the files in the blog application

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 import express from "express"; 
 import bodyParser from "body-parser"; 
+import morgan from "morgan"; 
 
 const app = express(); 
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("common")) 
-
+app.use(express.static("public"))
 
 app.get("/", (req,res) => {
-    res.send("<h1>Blog Home Page<h1>"); 
+    res.render("index.ejs",); 
 }); 
 
+app.get("/about", (req, res) => {
+    res.render("about.ejs"); 
+})
 
-app.listen(3000, ()=>{
-    console.log(`app is running on port 3000`)
+app.get("/contect", (req, res) => {
+    res.render("contect.ejs")
+})
+
+app.listen(port, ()=>{
+    console.log(`app is running on port ${port}`)
 }); 
